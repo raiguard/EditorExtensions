c05f74d4c9ce6b4be6edcebcf09cbbe57fc1acff
raiguard <caleb.heuer@gmail.com>
2019-11-28 23:37:57 -0700

    Remove free ore recipes

--- src/data-updates.lua
+++ src/data-updates.lua
@@ -1,25 +1,3 @@
--- add free crafting recipes for all ores
-register_recipes({'wood'}, true)
-for n,t in pairs(data.raw['resource']) do
-    if t.minable then
-        local results = {}
-        if t.minable.result then
-            results[1] = data.raw['item'][t.minable.result]
-        elseif t.minable.results then
-            for i,r in pairs(t.minable.results) do
-                results[i] = data.raw['item'][r.name]
-            end
-        end
-        for _,result in pairs(results) do
-            local recipe = data.raw['recipe'][result.name]
-            if result.type == 'item' and not recipe then
-                result.subgroup = 'raw-resource'
-                register_recipes({result.name}, true)
-            end
-        end
-    end
-end
-
 -- create infinity loader loaders
 local loader_base = table.deepcopy(data.raw['underground-belt']['underground-belt'])
 loader_base.icons = {apply_infinity_tint{icon='__EditorExtensions__/graphics/item/infinity-loader.png', icon_size=32}}
