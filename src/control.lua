3ee2359cf712279b602583d034f6f7bfdb424788
raiguard <caleb.heuer@gmail.com>
2019-11-29 11:36:03 -0700

    Consolidate infinity inserter code into main file

--- src/control.lua
+++ src/control.lua
@@ -5,7 +5,6 @@
 -- MODULES
 
 require('scripts/infinity-accumulator')
-require('scripts/infinity-inserter')
 require('scripts/infinity-loader')
 require('scripts/infinity-wagon')
 require('scripts/tesseract-chest')
@@ -49,3 +48,22 @@ event.register({defines.events.on_lua_shortcut, 'ee-toggle-map-editor'}, functio
         game.tick_paused = false
     end
 end)
+
+-- --------------------------------------------------
+-- INFINITY INSERTER
+
+event.register(defines.events.on_built_entity, function(e)
+    local entity = e.created_entity
+    if entity.name == 'infinity-inserter' then
+        local control = entity.get_control_behavior()
+        if not control then
+            -- this is a new inserter, so set control mode to blacklist by default
+            entity.inserter_filter_mode = 'blacklist'
+        end
+    end
+end)
+
+-- --------------------------------------------------
+-- INFINITY PIPE
+
+-- event.register(defines.events.on_built_entity, )
