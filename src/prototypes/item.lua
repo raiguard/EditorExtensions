de318d637dcf477d90aceb2bb9ee9328bae33089
raiguard <caleb.heuer@gmail.com>
2019-12-22 16:24:14 -0700

    Mark all items as hidden

--- src/prototypes/item.lua
+++ src/prototypes/item.lua
@@ -11,7 +11,8 @@ data:extend{
     icons = recursive_tint{extract_icon_info(data.raw['accumulator']['accumulator'])},
     place_result = 'infinity-accumulator-primary-output',
     subgroup = 'ee-electricity',
-    order = 'a'
+    order = 'a',
+    flags = {'hidden'}
   }
 }
 
@@ -22,6 +23,7 @@ infinity_beacon.icons = recursive_tint{extract_icon_info(infinity_beacon)}
 infinity_beacon.place_result = 'infinity-beacon'
 infinity_beacon.subgroup='ee-modules'
 infinity_beacon.order = 'aa'
+infinity_beacon.flags = {'hidden'}
 data:extend{infinity_beacon}
 
 -- INFINITY AND TESSERACT CHESTS
@@ -31,7 +33,7 @@ do
   ic_item.subgroup = 'ee-inventories'
   ic_item.order = 'aa'
   ic_item.stack_size = 50
-  ic_item.flags = {}
+  ic_item.flags = {'hidden'}
 
   -- create logistic chest items
   ic_item = table.deepcopy(data.raw['item']['infinity-chest'])
@@ -42,7 +44,6 @@ do
     chest.icons = {{icon=chest.icon, icon_size=chest.icon_size, icon_mipmaps=chest.icon_mipmaps, tint=d.t}}
     chest.place_result = 'infinity-chest-' .. lm
     chest.order = d.o
-    chest.flags = {}
     data:extend{chest}
   end
 
@@ -71,7 +72,8 @@ data:extend{
     icons = recursive_tint({extract_icon_info(data.raw['constant-combinator']['constant-combinator'])}, combinator_tint),
     place_result = 'infinity-combinator',
     subgroup = 'ee-electricity',
-    order = 'z'
+    order = 'z',
+    flags = {'hidden'}
   }
 }
 
@@ -85,7 +87,8 @@ data:extend{
     subgroup = 'ee-equipment',
     order = 'aa',
     placed_as_equipment_result = 'infinity-fusion-reactor-equipment',
-    stack_size = 50
+    stack_size = 50,
+    flags = {'hidden'}
   }
 }
 
@@ -96,12 +99,14 @@ infinity_electric_pole.icons = recursive_tint{extract_icon_info(infinity_electri
 infinity_electric_pole.place_result = 'infinity-electric-pole'
 infinity_electric_pole.subgroup = 'ee-electricity'
 infinity_electric_pole.order = 'ba'
+infinity_electric_pole.flags = {'hidden'}
 local infinity_substation = table.deepcopy(data.raw['item']['substation'])
 infinity_substation.name = 'infinity-substation'
 infinity_substation.icons = recursive_tint{extract_icon_info(infinity_substation)}
 infinity_substation.place_result = 'infinity-substation'
 infinity_substation.subgroup = 'ee-electricity'
 infinity_substation.order = 'bb'
+infinity_substation.flags = {'hidden'}
 data:extend{infinity_electric_pole, infinity_substation}
 
 -- INFINITY FUEL
@@ -112,6 +117,7 @@ infinity_fuel.stack_size = 100
 infinity_fuel.fuel_value = '1000YJ'
 infinity_fuel.subgroup = 'ee-trains'
 infinity_fuel.order = 'c'
+infinity_fuel.flags = {'hidden'}
 data:extend{infinity_fuel}
 
 -- INFINITY HEAT PIPE
@@ -120,7 +126,7 @@ infinity_heat_pipe.subgroup = 'ee-misc'
 infinity_heat_pipe.order = 'ca'
 infinity_heat_pipe.stack_size = 50
 infinity_heat_pipe.icons = recursive_tint{extract_icon_info(data.raw['item']['heat-pipe'])}
-infinity_heat_pipe.flags = {}
+infinity_heat_pipe.flags = {'hidden'}
 
 -- INFINITY INSERTER
 local infinity_inserter = table.deepcopy(data.raw['item']['filter-inserter'])
@@ -129,6 +135,7 @@ infinity_inserter.icons = recursive_tint{extract_icon_info(infinity_inserter)}
 infinity_inserter.place_result = 'infinity-inserter'
 infinity_inserter.subgroup = 'ee-misc'
 infinity_inserter.order = 'ab'
+infinity_inserter.flags = {'hidden'}
 data:extend{infinity_inserter}
 
 -- INFINITY LAB
@@ -138,6 +145,7 @@ infinity_lab.icons = recursive_tint{extract_icon_info(infinity_lab)}
 infinity_lab.place_result = 'infinity-lab'
 infinity_lab.subgroup = 'ee-misc'
 infinity_lab.order = 'ea'
+infinity_lab.flags = {'hidden'}
 data:extend{infinity_lab}
 
 -- INFINITY LOADER
@@ -150,7 +158,8 @@ data:extend{
     stack_size = 50,
     place_result = 'infinity-loader-dummy-combinator',
     subgroup = 'ee-misc',
-    order = 'aa'
+    order = 'aa',
+    flags = {'hidden'}
   }
 }
 
@@ -162,6 +171,7 @@ infinity_locomotive.place_result = 'infinity-locomotive'
 infinity_locomotive.subgroup = 'ee-trains'
 infinity_locomotive.order = 'aa'
 infinity_locomotive.stack_size = 50
+infinity_locomotive.flags = {'hidden'}
 data:extend{infinity_locomotive}
 
 -- INFINITY PERSONAL ROBOPORT
@@ -174,7 +184,8 @@ data:extend{
     subgroup = 'ee-equipment',
     order = 'ab',
     placed_as_equipment_result = 'infinity-personal-roboport-equipment',
-    stack_size = 50
+    stack_size = 50,
+    flags = {'hidden'}
   }
 }
 
@@ -184,7 +195,7 @@ infinity_pipe.icons = recursive_tint{infinity_pipe.icons[1]}
 infinity_pipe.subgroup = 'ee-misc'
 infinity_pipe.order = 'ba'
 infinity_pipe.stack_size = 50
-infinity_pipe.flags = {}
+infinity_pipe.flags = {'hidden'}
 
 -- INFINITY PUMP
 local infinity_pump = table.deepcopy(data.raw['item']['pump'])
@@ -193,6 +204,7 @@ infinity_pump.icons = recursive_tint{extract_icon_info(infinity_pump)}
 infinity_pump.place_result = 'infinity-pump'
 infinity_pump.subgroup = 'ee-misc'
 infinity_pump.order = 'bb'
+infinity_pump.flags = {'hidden'}
 data:extend{infinity_pump}
 
 -- INFINITY RADAR
@@ -202,6 +214,7 @@ infinity_radar.icons = recursive_tint{extract_icon_info(infinity_radar)}
 infinity_radar.place_result = 'infinity-radar'
 infinity_radar.subgroup = 'ee-misc'
 infinity_radar.order = 'da'
+infinity_radar.flags = {'hidden'}
 data:extend{infinity_radar}
 
 -- INFINITY ROBOPORT
@@ -212,6 +225,7 @@ infinity_roboport.place_result = 'infinity-roboport'
 infinity_roboport.subgroup = 'ee-robots'
 infinity_roboport.order = 'a'
 infinity_roboport.stack_size = 50
+infinity_roboport.flags = {'hidden'}
 data:extend{infinity_roboport}
 
 -- INFINITY ROBOTS
@@ -222,6 +236,7 @@ infinity_construction_robot.place_result = 'infinity-construction-robot'
 infinity_construction_robot.subgroup = 'ee-robots'
 infinity_construction_robot.order = 'ba'
 infinity_construction_robot.stack_size = 100
+infinity_construction_robot.flags = {'hidden'}
 local infinity_logistic_robot = table.deepcopy(data.raw['item']['logistic-robot'])
 infinity_logistic_robot.name = 'infinity-logistic-robot'
 infinity_logistic_robot.icons = recursive_tint{extract_icon_info(infinity_logistic_robot)}
@@ -229,6 +244,7 @@ infinity_logistic_robot.place_result = 'infinity-logistic-robot'
 infinity_logistic_robot.subgroup = 'ee-robots'
 infinity_logistic_robot.order = 'bb'
 infinity_logistic_robot.stack_size = 100
+infinity_logistic_robot.flags = {'hidden'}
 data:extend{infinity_construction_robot, infinity_logistic_robot}
 
 -- INFINITY WAGONS
@@ -239,6 +255,7 @@ infinity_cargo_wagon.place_result = 'infinity-cargo-wagon'
 infinity_cargo_wagon.subgroup = 'ee-trains'
 infinity_cargo_wagon.order = 'ba'
 infinity_cargo_wagon.stack_size = 50
+infinity_cargo_wagon.flags = {'hidden'}
 local infinity_fluid_wagon = table.deepcopy(data.raw['item-with-entity-data']['fluid-wagon'])
 infinity_fluid_wagon.name = 'infinity-fluid-wagon'
 infinity_fluid_wagon.icons = recursive_tint{extract_icon_info(infinity_fluid_wagon)}
@@ -246,4 +263,5 @@ infinity_fluid_wagon.place_result = 'infinity-fluid-wagon'
 infinity_fluid_wagon.subgroup = 'ee-trains'
 infinity_fluid_wagon.order = 'bb'
 infinity_fluid_wagon.stack_size = 50
+infinity_fluid_wagon.flags = {'hidden'}
 data:extend{infinity_cargo_wagon, infinity_fluid_wagon}
