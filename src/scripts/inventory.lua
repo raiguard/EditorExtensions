c8a00051dda47fb381db02bc92803ef66fd9c9e9
raiguard <caleb.heuer@gmail.com>
2020-02-16 16:47:50 -0700

    Remove unfiltered items support

--- src/scripts/inventory.lua
+++ src/scripts/inventory.lua
@@ -36,45 +36,40 @@ local string_sub = string.sub
 local filters_table_version = 0
 local filters_table_migrations = {}
 
-local test_string = 'EditorExtensions-inventory_filters-0-[{"name":"enriched-fuel","count":20,"mode":"exactly","index":1},{"name":"speed-module-3","count":100,"mode":"at-least","index":2},{"name":"ultimate-transport-belt","count":100,"mode":"at-most","index":3},{"name":"sapphire-5","count":1000,"mode":"exactly","index":4},{"name":"infinity-loader","count":50,"mode":"at-least","index":5},{"name":"logistic-chest-passive-provider","count":50,"mode":"at-least","index":6}]'
-
-local TEMP_FILTERS = {
-  {name='express-transport-belt', count=100, mode='exactly', index=1},
-  {name='express-underground-belt', count=100, mode='exactly', index=2},
-  {name='express-splitter', count=100, mode='exactly', index=3},
-  {name='medium-electric-pole', count=50, mode='exactly', index=4},
-  {name='big-electric-pole', count=50, mode='at-most', index=5},
-  {name='substation', count=50, mode='at-least', index=6},
-}
-
 local function export_filters(player)
-  return 'EditorExtensions-inventory_filters-'..filters_table_version..'-'..game.table_to_json(player.infinity_inventory_filters)
+  local filters = player.infinity_inventory_filters
+  local output = {
+    filters = filters,
+    remove_unfiltered_items = player.remove_unfiltered_items
+  }
+  return 'EditorExtensions-inventory_filters-'..filters_table_version..'-'..game.table_to_json(output)
 end
 
 local function import_filters(player, string)
   -- local decoded_string = game.decode_string(string)
-  -- local decoded_string = 'EditorExtensions-inventory_filters-0-'..game.table_to_json(TEMP_FILTERS)
-  local decoded_string = test_string
+  local decoded_string = string
   if string_sub(decoded_string, 1, 16) == 'EditorExtensions' and string_sub(decoded_string, 18, 34) == 'inventory_filters' then
     -- extract version for migrations
     local _,_,version,json = string_find(decoded_string, '^.-%-.-%-(%d-)%-(.*)$')
     version = tonumber(version)
-    local filters_table = game.json_to_table(json)
+    local input = game.json_to_table(json)
     if version < filters_table_version then
-      migrations.generic(version, filters_table_migrations, filters_table)
+      migrations.generic(version, filters_table_migrations, input)
     end
     -- sanitise the filters to only include currently existing prototypes
     local item_prototypes = game.item_prototypes
     local output = {}
     local output_index = 0
-    for i=1,#filters_table do
-      local filter = filters_table[i]
+    local filters = input.filters
+    for i=1,#filters do
+      local filter = filters[i]
       if item_prototypes[filter.name] then
         output_index = output_index + 1
         output[output_index] = {name=filter.name, count=filter.count, mode=filter.mode, index=output_index}
       end
     end
     player.infinity_inventory_filters = output
+    player.remove_unfiltered_items = input.remove_unfiltered_items
     return true
   end
   return false
@@ -110,8 +105,8 @@ gui.add_handlers{
               {type='label', style='frame_title', caption={'ee-gui.'..mode..'-inventory-filters'}},
               {type='empty-widget', style={name='draggable_space_header', height=24, horizontally_stretchable=true}, save_as='drag_handle'}
             }},
-            {type='text-box', style={width=400, height=300}, clear_and_focus_on_right_click=true, handlers=(mode == 'import' and 'textbox' or nil),
-              save_as='textbox'},
+            {type='text-box', style={width=400, height=300}, clear_and_focus_on_right_click=true, mods={word_wrap=true},
+              handlers=(mode == 'import' and 'textbox' or nil), save_as='textbox'},
             {template='inventory_filters_string.'..mode..'_nav_flow'}
           }}
         )
