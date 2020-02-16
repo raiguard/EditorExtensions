3ed794dc43c180b97ba22cc0cdbf7b281aac6e9b
raiguard <caleb.heuer@gmail.com>
2020-02-15 20:55:20 -0700

    Consolidate sprites

--- src/prototypes/sprite.lua
+++ src/prototypes/sprite.lua
@@ -1,11 +1,12 @@
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- SPRITES
 
-local sort_sprite = function(mode, direction)
+local sort_sprite = function(name, y)
   return {
     type = 'sprite',
-    name = 'ee-sort-'..mode..'-'..direction,
-    filename = '__EditorExtensions__/graphics/gui/sort-'..mode..'-'..direction..'.png',
+    name = 'ee-sort-'..name,
+    filename = '__EditorExtensions__/graphics/gui/infinity-combinator.png',
+    y = y,
     size = 32,
     mipmap_count = 2,
     flags = {'icon'}
@@ -24,7 +25,8 @@ data:extend{
   {
     type = 'sprite',
     name = 'ee-time',
-    filename = '__EditorExtensions__/graphics/gui/time-alt.png',
+    filename = '__EditorExtensions__/graphics/gui/infinity-combinator.png',
+    y = 0,
     size = 32,
     mipmap_count = 2,
     flags = {'icon'}
@@ -32,15 +34,16 @@ data:extend{
   {
     type = 'sprite',
     name = 'ee-sort',
-    filename = '__EditorExtensions__/graphics/gui/sort.png',
+    filename = '__EditorExtensions__/graphics/gui/infinity-combinator.png',
+    y = 32,
     size = 32,
     mipmap_count = 2,
     flags = {'icon'}
   },
-  sort_sprite('alphabetical', 'ascending'),
-  sort_sprite('alphabetical', 'descending'),
-  sort_sprite('numerical', 'ascending'),
-  sort_sprite('numerical', 'descending'),
+  sort_sprite('alphabetical-ascending', 64),
+  sort_sprite('alphabetical-descending', 96),
+  sort_sprite('numerical-ascending', 128),
+  sort_sprite('numerical-descending', 160),
   {
     type = 'sprite',
     name = 'ee-import-inventory-filters',
