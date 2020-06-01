08e0b9294c81974cd1cbd07f2c1724885d478efe
raiguard <caleb.heuer@gmail.com>
2020-05-31 19:24:10 -0600

    Include world with testing scenario

--- src/scenarios/testing/control.lua
+++ src/scenarios/testing/control.lua
@@ -4,25 +4,19 @@ end
 
 local event = require("__flib__.event")
 
-event.on_init(function()
-  -- set lab tiles
-  local nauvis = game.surfaces.nauvis
-  nauvis.generate_with_lab_tiles = true
-  nauvis.clear(true)
-
-  -- freeze time at noonday
-  nauvis.freeze_daytime = true
-  nauvis.daytime = 0
-end)
-
 event.on_player_created(function(e)
-  if not global.message_shown then
-    global.message_shown = true
-    if game.is_multiplayer() then
-      game.get_player(e.player_index).print{"description"}
-    else
-      game.show_message_dialog{text={"description"}, point_to={type="position", position={0,-1.5}}}
-    end
+  local player = game.get_player(e.player_index)
+
+  -- chart area
+  local player_pos = player.position
+  local radius = 200
+  player.force.chart(player.surface, {{player_pos.x - radius, player_pos.y - radius}, {player_pos.x + radius, player_pos.y + radius}})
+
+  -- show message
+  if game.is_multiplayer() then
+    player.print{"description"}
+  else
+    game.show_message_dialog{text={"description"}, point_to={type="position", position={0,-1.5}}}
   end
 end)
 
