5a0db735fed1b99ad451946c29576eff79a50d62
raiguard <caleb.heuer@gmail.com>
2020-08-23 15:40:56 -0600

    Fix infinity pipe snapping

--- src/scripts/entity/infinity-pipe.lua
+++ src/scripts/entity/infinity-pipe.lua
@@ -1,34 +1,49 @@
 local infinity_pipe = {}
 
+local pipe_snapping_types = {
+  ["infinity-pipe"] = true,
+  ["offshore-pump"] = true,
+  ["pipe-to-ground"] = true,
+  ["pipe"] = true,
+  ["pump"] = true,
+  ["storage-tank"] = true
+}
+
 function infinity_pipe.snap(entity, player_settings)
   local neighbours = entity.neighbours[1]
-  local own_fb = entity.fluidbox
   local own_id = entity.unit_number
-  -- snap to adjacent assemblers
-  if player_settings.infinity_pipe_assembler_snapping then
+  -- settings
+  local assembler_snapping = player_settings.infinity_pipe_assembler_snapping
+  local pipe_snapping = player_settings.infinity_pipe_snapping
+
+  if assembler_snapping or pipe_snapping then
     for ni=1, #neighbours do
       local neighbour = neighbours[ni]
-      if neighbour.type == "assembling-machine" and neighbour.fluidbox then
-        local fb = neighbour.fluidbox
-        for i=1, #fb do
-          local connections = fb.get_connections(i)
-          if connections[1] and (connections[1].owner.unit_number == own_id) and (fb.get_prototype(i).production_type == "input") then
-            local fluid = own_fb.get_locked_fluid(1)
-            if fluid then
-              -- set to fill the pipe with the fluid
-              entity.set_infinity_pipe_filter{name=fluid, percentage=1, mode="exactly"}
+      local fb = neighbour.fluidbox
+      if fb then
+        -- snap to adjacent assemblers
+        if assembler_snapping and neighbour.type == "assembling-machine" then
+          for i = 1, #fb do
+            local connections = fb.get_connections(i)
+            if connections[1] and (connections[1].owner.unit_number == own_id) and (fb.get_prototype(i).production_type == "input") then
+              local fluid = fb.get_locked_fluid(1)
+              if fluid then
+                -- set to fill the pipe with the fluid
+                entity.set_infinity_pipe_filter{name=fluid, percentage=1, mode="exactly"}
+              end
+              return -- break
             end
-            return -- don't do default snapping
+          end
+        -- snap to locked fluid
+        elseif pipe_snapping and pipe_snapping_types[neighbour.type] then
+          local fluid = fb[1]
+          if fluid then
+            -- set filter to that fluid, but don't do anything with it
+            entity.set_infinity_pipe_filter{name=fluid.name, percentage=0, mode="at-least"}
+            return -- break
           end
         end
       end
-    end
-  end
-  -- snap to locked fluid
-  if player_settings.infinity_pipe_snapping then
-    local fluid = own_fb.get_locked_fluid(1)
-    if fluid then
-      entity.set_infinity_pipe_filter{name=fluid, percentage=0, mode="exactly"}
     end
   end
 end
