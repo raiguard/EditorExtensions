d61c93dba50b42b1873bda75f22b8bea1034cd9a
raiguard <caleb.heuer@gmail.com>
2020-08-23 16:25:51 -0600

    Greatly optimize aggregate chest setup

--- src/scripts/entity/aggregate-chest.lua
+++ src/scripts/entity/aggregate-chest.lua
@@ -8,14 +8,10 @@ local chest_names = {
 -- set the filters for the given aggregate chest
 function aggregate_chest.set_filters(entity)
   entity.remove_unfiltered_items = true
-  local i = 0
-  for name, stack_size in pairs(global.aggregate_data) do
-    i = i + 1
-    entity.set_infinity_container_filter(i, {name=name, count=stack_size, mode="exactly", index=i})
-  end
+  entity.infinity_container_filters = global.aggregate_filters
 end
 
--- updates the filters of all existing aggregate chests
+-- update the filters of all existing aggregate chests
 function aggregate_chest.update_all_filters()
   for _, surface in pairs(game.surfaces) do
     for _, entity in pairs(surface.find_entities_filtered{name=chest_names}) do
@@ -28,12 +24,14 @@ end
 function aggregate_chest.update_data()
   local include_hidden = settings.global["ee-aggregate-include-hidden"].value
   local data = {}
+  local i = 0
   for name, prototype in pairs(game.item_prototypes) do
     if prototype.type ~= "mining-tool" and include_hidden or not prototype.has_flag("hidden") then
-      data[name] = prototype.stack_size
+      i = i + 1
+      data[i] = {name=name, count=prototype.stack_size, mode="exactly", index=i}
     end
   end
-  global.aggregate_data = data
+  global.aggregate_filters = data
 end
 
 return aggregate_chest
