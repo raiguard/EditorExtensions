15205eac9b7ca10de8e420a5aa56e274cc7b0d9b
raiguard <caleb.heuer@gmail.com>
2020-09-07 11:26:16 -0600

    Ordering

--- src/scripts/compatibility.lua
+++ src/scripts/compatibility.lua
@@ -4,14 +4,14 @@ local event = require("__flib__.event")
 
 local infinity_loader = require("scripts.entity.infinity-loader")
 
+function compatibility.check_space_exploration()
+  return script.active_mods["space-exploration"]
+end
+
 function compatibility.register_picker_dollies()
   if script.active_mods["PickerDollies"] then
     event.register(remote.call("PickerDollies", "dolly_moved_entity_id"), infinity_loader.picker_dollies_move)
   end
-end
-
-function compatibility.check_space_exploration()
-  return script.active_mods["space-exploration"]
 end
 
 return compatibility
