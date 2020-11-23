9131c994361238a863bcb9c337db79b889a30013
raiguard <caleb.heuer@gmail.com>
2020-11-23 15:27:11 -0700

    Fix typo in scenario error message

--- src/scenarios/testing/control.lua
+++ src/scenarios/testing/control.lua
@@ -5,5 +5,5 @@ end
 if script.active_mods["EditorExtensions"] then
   require("__EditorExtensions__.scripts.scenarios.testing")
 else
-  error("Editor Extensions and must be active for this scenario to function.")
+  error("Editor Extensions must be active for this scenario to function.")
 end
