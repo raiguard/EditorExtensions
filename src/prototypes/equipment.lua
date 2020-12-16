c135cda21b3a0885c2d4b0e8077d9bba9393aebc
raiguard <caleb.heuer@gmail.com>
2020-12-15 20:32:51 -0700

    Fix cheat equipment tinting

--- src/prototypes/equipment.lua
+++ src/prototypes/equipment.lua
@@ -1,17 +1,18 @@
 local constants = require("prototypes.constants")
+local util = require("prototypes.util")
 
 local personal_battery = table.deepcopy(data.raw["battery-equipment"]["battery-equipment"])
 personal_battery.name = "ee-super-battery-equipment"
 personal_battery.background_color = constants.equipment_background_color
 personal_battery.grabbed_background_color = constants.equipment_background_color_hovered
 personal_battery.shape = {width = 1, height = 1, type = "full"}
-personal_battery.sprite.tint = constants.infinity_tint
 personal_battery.take_result = "ee-super-battery-equipment"
 personal_battery.energy_source.buffer_capacity = "1000YJ"
+util.recursive_tint(personal_battery)
 data:extend{personal_battery}
 
 data:extend{
-  -- infinity exoskeleton
+  -- super exoskeleton
   {
     type = "movement-bonus-equipment",
     name = "ee-super-exoskeleton-equipment",
@@ -54,7 +55,6 @@ local energy_shield = table.deepcopy(data.raw["energy-shield-equipment"]["energy
 energy_shield.name = "ee-super-energy-shield-equipment"
 energy_shield.background_color = constants.equipment_background_color
 energy_shield.grabbed_background_color = constants.equipment_background_color_hovered
-energy_shield.sprite.tint = constants.infinity_tint
 energy_shield.shape = {width = 1, height = 1, type = "full"}
 energy_shield.max_shield_value = 1000000
 energy_shield.energy_source = {
@@ -64,17 +64,18 @@ energy_shield.energy_source = {
   buffer_capacity = "100YJ"
 }
 energy_shield.take_result = "ee-super-energy-shield-equipment"
+util.recursive_tint(energy_shield)
 data:extend{energy_shield}
 
 local night_vision = table.deepcopy(data.raw["night-vision-equipment"]["night-vision-equipment"])
 night_vision.name = "ee-super-night-vision-equipment"
 night_vision.background_color = constants.equipment_background_color
 night_vision.grabbed_background_color = constants.equipment_background_color_hovered
-night_vision.sprite.tint = constants.infinity_tint
 night_vision.shape = {width = 1, height = 1, type = "full"}
 night_vision.darkness_to_turn_on = 0
 night_vision.color_lookup = {{0.5, "__core__/graphics/color_luts/identity-lut.png"}}
 night_vision.take_result = "ee-super-night-vision-equipment"
+util.recursive_tint(night_vision)
 data:extend{night_vision}
 
 local personal_roboport = table.deepcopy(data.raw["roboport-equipment"]["personal-roboport-mk2-equipment"])
@@ -82,7 +83,6 @@ personal_roboport.name = "ee-super-personal-roboport-equipment"
 personal_roboport.background_color = constants.equipment_background_color
 personal_roboport.grabbed_background_color = constants.equipment_background_color_hovered
 personal_roboport.shape = {width = 1, height = 1, type = "full"}
-personal_roboport.sprite.tint = constants.infinity_tint
 personal_roboport.charging_energy = "1000GJ"
 personal_roboport.charging_station_count = 1000
 personal_roboport.robot_limit = 1000
@@ -94,4 +94,5 @@ personal_roboport.energy_source = {
   input_flow_limit = "100YW"
 }
 personal_roboport.take_result = "ee-super-personal-roboport-equipment"
+util.recursive_tint(personal_roboport)
 data:extend{personal_roboport}
