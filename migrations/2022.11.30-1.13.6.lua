98555fdf33fe3edb02342f9bc00645477a27b325
Caleb Heuer <ch@raiguard.me>
2022-11-30 23:33:31 -0700

    Move scenario script into the scenario

---
+++ migrations/2022.11.30-1.13.6.lua
@@ -0,0 +1,3 @@
+if script.level.mod_name == "EditorExtensions" and script.level.level_name == "testing" then
+  game.reload_script()
+end
--- scenarios/testing/control.lua
+++ scenarios/testing/control.lua
@@ -1,30 +1,83 @@
-if not script.active_mods["EditorExtensions"] then
-  error("Editor Extensions must be active for this scenario to function.")
+local migration = require("__flib__/migration")
+local mod_gui = require("__core__/lualib/mod-gui")
+
+local callbacks = {}
+
+--- @param force LuaForce
+local function setup_force(force)
+  force.research_all_technologies()
+
+  force.max_successful_attempts_per_tick_per_construction_queue = 30
+  force.max_failed_attempts_per_tick_per_construction_queue = 10
 end
 
-local callbacks = require("__EditorExtensions__/scripts/scenarios/testing")
+-- migrations are based on EE versions
+local migrations = {
+  ["1.5.18"] = function()
+    -- add bot bonuses to all forces
+    for _, force in pairs(game.forces) do
+      force.max_successful_attempts_per_tick_per_construction_queue = 30
+      force.max_failed_attempts_per_tick_per_construction_queue = 10
+    end
+  end,
+  ["1.8.3"] = function()
+    -- remove rocket silo GUI
+    for _, player in pairs(game.players) do
+      local button = mod_gui.get_button_flow(player).silo_gui_sprite_button
+      if button then
+        button.destroy()
+      end
+      local frame = mod_gui.get_frame_flow(player).silo_gui_frame
+      if frame then
+        frame.destroy()
+      end
+    end
+  end,
+}
 
---[[
-  MOD DEVELOPERS:
-  To create a custom mod testing scenario, navigate to map editor -> new scenario and choose this scenario as your
-  template. Build whatever testing setups you need, then save it to your user scenarios. You can then choose it from
-  the new game menu!
+script.on_init(function()
+  if remote.interfaces["kr-crash-site"] then
+    remote.call("kr-crash-site", "crash_site_enabled", false)
+  end
 
-  If you need custom scripting (for setting up player defaults, etc.), you can add your own code below. The EE scenario
-  controls the following event handlers:
-    on_init
-    on_configuration_changed
-    on_force_created
-    on_player_created
+  for _, force in pairs(game.forces) do
+    setup_force(force)
+  end
 
-  If you need to run code in these events, add callbacks for them to the provided `callbacks` table:
+  if callbacks.on_init then
+    callbacks.on_init()
+  end
+end)
 
-  callbacks.on_init = function() log("init!") end
+script.on_configuration_changed(function(e)
+  migration.on_config_changed(e, migrations, "EditorExtensions")
 
-  All other event handlers may be registered however you like.
+  if callbacks.on_configuration_changed then
+    callbacks.on_configuration_changed(e)
+  end
+end)
 
-  The EE scenario does not use `global`, so feel free to use it if you need it.
-]]
+script.on_event(defines.events.on_force_created, function(e)
+  setup_force(e.force)
 
--- so the language server doesn't yell at me
-callbacks.on_init = nil
+  if callbacks.on_force_created then
+    callbacks.on_force_created(e)
+  end
+end)
+
+script.on_event(defines.events.on_player_created, function(e)
+  game.get_player(e.player_index).cheat_mode = true
+
+  if callbacks.on_player_created then
+    callbacks.on_player_created(e)
+  end
+end)
+
+remote.add_interface("EditorExtensions_TestingScenario", {
+  -- disable AAI industry crash site
+  allow_aai_crash_sequence = function()
+    return { allow = false, weight = 1 }
+  end,
+})
+
+return callbacks
