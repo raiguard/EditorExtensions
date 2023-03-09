5b9230284f3b448886f6e2e329a36f558a78ec0e
Caleb Heuer <ch@raiguard.me>
2023-03-08 23:38:18 -0700

    Preserve infinity wagon state

--- scripts/migrations.lua
+++ scripts/migrations.lua
@@ -4,7 +4,6 @@ local aggregate_chest = require("__EditorExtensions__/scripts/aggregate-chest")
 local infinity_accumulator = require("__EditorExtensions__/scripts/infinity-accumulator")
 local infinity_loader = require("__EditorExtensions__/scripts/infinity-loader")
 local infinity_pipe = require("__EditorExtensions__/scripts/infinity-pipe")
-local infinity_wagon = require("__EditorExtensions__/scripts/infinity-wagon")
 local inventory_filters = require("__EditorExtensions__/scripts/inventory-filters")
 local inventory_sync = require("__EditorExtensions__/scripts/inventory-sync")
 local linked_belt = require("__EditorExtensions__/scripts/linked-belt")
@@ -25,7 +24,7 @@ local version_migrations = {
       end
     end
     -- NUKE EVERYTHING
-    global = { testing_lab_state = testing_lab_state }
+    global = { testing_lab_state = testing_lab_state, wagons = global.wagons }
     rendering.clear("EditorExtensions")
     for _, player in pairs(game.players) do
       for _, gui in pairs({ player.gui.top, player.gui.left, player.gui.center, player.gui.screen, player.gui.relative }) do
@@ -41,7 +40,6 @@ local version_migrations = {
     infinity_accumulator.on_init()
     infinity_loader.on_init()
     infinity_pipe.on_init()
-    infinity_wagon.on_init()
     inventory_filters.on_init()
     inventory_sync.on_init()
     linked_belt.on_init()
