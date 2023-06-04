657cbea5e995255ce544eea8d66bbc5ca9299f2c
Caleb Heuer <ch@raiguard.me>
2023-06-04 12:02:11 -0600

    Clean up control.lua

--- control.lua
+++ control.lua
@@ -1,22 +1,24 @@
 local handler = require("__core__/lualib/event_handler")
 
-handler.add_lib(require("__flib__/gui-lite"))
+handler.add_libraries({
+  require("__flib__/gui-lite"),
 
-handler.add_lib(require("__EditorExtensions__/scripts/migrations"))
+  require("__EditorExtensions__/scripts/migrations"),
 
-handler.add_lib(require("__EditorExtensions__/scripts/aggregate-chest"))
-handler.add_lib(require("__EditorExtensions__/scripts/cheat-mode"))
-handler.add_lib(require("__EditorExtensions__/scripts/debug-world"))
-handler.add_lib(require("__EditorExtensions__/scripts/editor"))
-handler.add_lib(require("__EditorExtensions__/scripts/infinity-accumulator"))
-handler.add_lib(require("__EditorExtensions__/scripts/infinity-loader"))
-handler.add_lib(require("__EditorExtensions__/scripts/infinity-pipe"))
-handler.add_lib(require("__EditorExtensions__/scripts/infinity-wagon"))
-handler.add_lib(require("__EditorExtensions__/scripts/inventory-filters"))
-handler.add_lib(require("__EditorExtensions__/scripts/inventory-sync"))
-handler.add_lib(require("__EditorExtensions__/scripts/linked-belt"))
-handler.add_lib(require("__EditorExtensions__/scripts/super-inserter"))
-handler.add_lib(require("__EditorExtensions__/scripts/super-pump"))
-handler.add_lib(require("__EditorExtensions__/scripts/testing-lab"))
+  require("__EditorExtensions__/scripts/aggregate-chest"),
+  require("__EditorExtensions__/scripts/cheat-mode"),
+  require("__EditorExtensions__/scripts/debug-world"),
+  require("__EditorExtensions__/scripts/editor"),
+  require("__EditorExtensions__/scripts/infinity-accumulator"),
+  require("__EditorExtensions__/scripts/infinity-loader"),
+  require("__EditorExtensions__/scripts/infinity-pipe"),
+  require("__EditorExtensions__/scripts/infinity-wagon"),
+  require("__EditorExtensions__/scripts/inventory-filters"),
+  require("__EditorExtensions__/scripts/inventory-sync"),
+  require("__EditorExtensions__/scripts/linked-belt"),
+  require("__EditorExtensions__/scripts/super-inserter"),
+  require("__EditorExtensions__/scripts/super-pump"),
+  require("__EditorExtensions__/scripts/testing-lab"),
 
-handler.add_lib(require("__EditorExtensions__/scripts/update-notification"))
+  require("__EditorExtensions__/scripts/update-notification"),
+})
