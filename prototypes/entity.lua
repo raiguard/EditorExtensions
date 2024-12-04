96a621a9af8e6e145ce6bd53dddf689d69143616
Caleb Heuer <ch@raiguard.me>
2024-12-03 23:59:46 -0700

    Infinity loader outputs stacked items
    Fixes #184

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -191,6 +191,7 @@ data:extend({
     container_distance = 0,
     filter_count = 2,
     per_lane_filters = true,
+    max_belt_stack_size = 255,
     structure = flib_table.deepcopy(data.raw["linked-belt"]["linked-belt"].structure),
     open_sound = sounds.machine_open,
     close_sound = sounds.machine_close,
